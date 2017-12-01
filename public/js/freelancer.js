a5ca4fe71acbabf5b12fbebc5b8e624d449c3d60
Surya Setiyaputra <hello@setiyaputra.me>
2017-12-02 01:28:21 +1100

    add request using ajaz

--- public/js/freelancer.js
+++ public/js/freelancer.js
@@ -1,26 +1,20 @@
-/* global TrelloPowerUp */
-var request = require("request");
-
-var myJSONObject =    { 
-     title: 'Write travel article',
-     description: 'Help me write December article',
-     currency: { code: 'AUD', id: 3, sign: '$' },
-     budget: { minimum: 500 },
-     jobs: [ { id: 2 }, { id: 174 } ] 
-};
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://www.freelancer-sandbox.com/api/projects/0.1/projects/?compact=",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+    "freelancer-oauth-v1": "vOmqi4FNxRFBRK7bdTkCq7JRZDKRpo",
+    "cache-control": "no-cache",
+    "postman-token": "beaca7d7-4bcd-2e6b-9e80-b570e9cd5af5"
+  },
+  "processData": false,
+  "data": "{\n  \"title\": \"Write trvel article\",\n  \"description\": \"Help me write December article\",\n  \"currency\": {\n        \"code\": \"AUD\",\n        \"id\": 3,\n        \"sign\": \"$\"\n    },\n    \"budget\": {\n        \"minimum\": 500\n    },\n    \"jobs\": [\n        {\n          \"id\": 2\n        },\n        {\n          \"id\": 174\n        }\n    ]\n}"
+}
 
 window.estimate.addEventListener('submit', function(event){
-  request({ method: 'POST',
-    url: 'https://www.freelancer-sandbox.com/api/projects/0.1/projects/',
-    qs: { compact: '' },
-    headers: 
-     { 'postman-token': '1760e9d2-2006-8e1a-ed64-fc7f21cca502',
-       'cache-control': 'no-cache',
-       'freelancer-oauth-v1': 'vOmqi4FNxRFBRK7bdTkCq7JRZDKRpo',
-       'content-type': 'application/json' },
-      json: true,   // <--Very important!!!
-      body: myJSONObject
-  }, function (error, response, body){
-      console.log(response);
+  $.ajax(settings).done(function (response) {
+    console.log(response);
   });
 });
