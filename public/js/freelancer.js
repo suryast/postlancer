3b5b30e051a8b24ffc0e1c958a60a3be618e9980
Surya Setiyaputra <hello@setiyaputra.me>
2017-12-02 00:43:37 +1100

    add request to freelancer API

--- public/js/freelancer.js
+++ public/js/freelancer.js
@@ -2,20 +2,31 @@
 
 var t = TrelloPowerUp.iframe();
 
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'https://www.freelancer-sandbox.com/api/projects/0.1/projects/',
+  qs: { compact: '' },
+  headers: 
+   { 'postman-token': '1760e9d2-2006-8e1a-ed64-fc7f21cca502',
+     'cache-control': 'no-cache',
+     'freelancer-oauth-v1': 'vOmqi4FNxRFBRK7bdTkCq7JRZDKRpo',
+     'content-type': 'application/json' },
+  body: 
+   { title: 'Write travel article 2nd',
+     description: 'Help me write December article',
+     currency: { code: 'AUD', id: 3, sign: '$' },
+     budget: { minimum: 500 },
+     jobs: [ { id: 2 }, { id: 174 } ] },
+  json: true };
+
 window.estimate.addEventListener('submit', function(event){
-  event.preventDefault();
-  return t.set('card', 'shared', 'estimate', window.estimateSize.value)
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
   .then(function(){
     t.closePopup();
-  });
-});
-
-t.render(function(){
-  return t.get('card', 'shared', 'estimate')
-  .then(function(estimate){
-    window.estimateSize.value = estimate;
-  })
-  .then(function(){
-    t.sizeTo('#estimate').done();
   });
 });
