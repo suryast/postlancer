4a41639f2055486d992527475e2617a42970b5c6
Surya Setiyaputra <hello@setiyaputra.me>
2017-12-01 17:38:24 +1100

    create static text badge

--- public/js/client.js
+++ public/js/client.js
@@ -50,20 +50,6 @@ TrelloPowerUp.initialize({
     .then(function(cardName){
       console.log('We just loaded the card name for fun: ' + cardName);
       return [{
-        // dynamic badges can have their function rerun
-        // after a set number of seconds defined by refresh.
-        // Minimum of 10 seconds.
-        dynamic: function(){
-          // we could also return a Promise that resolves to
-          // this as well if we needed to do something async first
-          return {
-            text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-            icon: './images/icon.svg',
-            color: 'green',
-            refresh: 10 // in seconds
-          };
-        }
-      }, {
         // its best to use static badges unless you need your
         // badges to refresh you can mix and match between
         // static and dynamic
@@ -92,7 +78,7 @@ TrelloPowerUp.initialize({
             });
           } else {
             return context.popup({
-              title: 'Change Snooze Time',
+              title: 'Outsource task',
               url: './freelancer.html',
               height: 411
             });
